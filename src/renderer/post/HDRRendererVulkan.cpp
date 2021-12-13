8f726adbf611cf70bfb3f93b994452f810aeca8d
Michael Ankele <michi@lupina.de>
2021-12-13 14:00:16 +0100

    refactoring HDRRendererVulkan

--- src/renderer/post/HDRRendererVulkan.cpp
+++ src/renderer/post/HDRRendererVulkan.cpp
@@ -33,30 +33,72 @@ RenderPass *blur_render_pass;
 static float resolution_scale_x = 1.0f;
 static float resolution_scale_y = 1.0f;
 
+static int BLUR_SCALE = 4;
 
-HDRRendererVulkan::HDRRendererVulkan(Renderer *parent) : PostProcessorStage("hdr", parent) {
-	ch_post_blur = PerformanceMonitor::create_channel("blur", channel);
-	ch_out = PerformanceMonitor::create_channel("out", channel);
+HDRRendererVulkan::RenderOutData::RenderOutData(Shader *s, Renderer *r, const Array<Texture*> &tex) {
+	shader_out = s;
+	pipeline_out = new vulkan::Pipeline(s, r->parent->render_pass(), 0, "3f,3f,2f");
+	pipeline_out->set_culling(0);
+	pipeline_out->rebuild();
+	dset_out = pool->create_set("buffer,sampler,sampler");
 
-	auto tex = new vulkan::DynamicTexture(width, height, 1, "rgba:f16");
-	_depth_buffer = new DepthBuffer(width, height, "d:f32", true);
+	foreachi (auto *t, tex, i)
+		dset_out->set_texture(1 + i, t);
+	dset_out->update();
+
+	vb_2d = new VertexBuffer("3f,3f,2f");
+	vb_2d->create_quad(rect::ID_SYM);
+}
+
+
+void HDRRendererVulkan::RenderOutData::render_out(CommandBuffer *cb, const Array<float> &data) {
+	vb_2d->create_quad(rect::ID_SYM, dynamicly_scaled_source());
+
+
+	cb->bind_pipeline(pipeline_out);
+	cb->bind_descriptor_set(0, dset_out);
+	struct PCOut {
+		matrix p, m, v;
+		float x[32];
+	};
+	PCOut pco = {matrix::ID, matrix::ID, matrix::ID, cam->exposure};
+	memcpy(&pco.x, &data[0], sizeof(float) * data.num);
+	cb->push_constant(0, sizeof(matrix) * 3 + sizeof(float) * data.num, &pco);
+	cb->draw(vb_2d);
+}
+
+
+HDRRendererVulkan::RenderIntoData::RenderIntoData(Renderer *r) {
+	auto tex = new vulkan::DynamicTexture(r->width, r->height, 1, "rgba:f16");
+	_depth_buffer = new DepthBuffer(r->width, r->height, "d:f32", true);
 	_render_pass = new vulkan::RenderPass({tex, _depth_buffer}, "clear");
 
 	fb_main = new vulkan::FrameBuffer(_render_pass, {
 		tex,
 		_depth_buffer});
 	fb_main->attachments[0]->set_options("wrap=clamp");
+}
 
 
-	auto blur_tex1 = new vulkan::DynamicTexture(width/2, height/2, 1, "rgba:f16");
-	auto blur_tex2 = new vulkan::DynamicTexture(width/2, height/2, 1, "rgba:f16");
-	auto blur_depth = new DepthBuffer(width/2, height/2, "d:f32", true);
+HDRRendererVulkan::HDRRendererVulkan(Renderer *parent) : PostProcessorStage("hdr", parent) {
+	ch_post_blur = PerformanceMonitor::create_channel("blur", channel);
+	ch_out = PerformanceMonitor::create_channel("out", channel);
+
+	into = RenderIntoData(this);
+
+
+	auto blur_tex1 = new vulkan::DynamicTexture(width/BLUR_SCALE, height/BLUR_SCALE, 1, "rgba:f16");
+	auto blur_tex2 = new vulkan::DynamicTexture(width/BLUR_SCALE, height/BLUR_SCALE, 1, "rgba:f16");
+	auto blur_depth = new DepthBuffer(width/BLUR_SCALE, height/BLUR_SCALE, "d:f32", true);
 	blur_tex1->set_options("wrap=clamp");
 	blur_tex2->set_options("wrap=clamp");
 
 	blur_render_pass = new vulkan::RenderPass({blur_tex1, blur_depth}, "clear");
+	// without clear, we get artifacts from dynamic resolution scaling
 	shader_blur = ResourceManager::load_shader("forward/blur.shader");
 	blur_pipeline = new vulkan::Pipeline(shader_blur.get(), blur_render_pass, 0, "3f,3f,2f");
+	blur_pipeline->set_z(false, false);
+	blur_pipeline->rebuild();
 	blur_ubo[0] = new UniformBuffer(sizeof(UBOBlur));
 	blur_ubo[1] = new UniformBuffer(sizeof(UBOBlur));
 	blur_dset[0] = pool->create_set(shader_blur.get());
@@ -64,11 +106,7 @@ HDRRendererVulkan::HDRRendererVulkan(Renderer *parent) : PostProcessorStage("hdr
 	fb_small1 = new vulkan::FrameBuffer(blur_render_pass, {blur_tex1, blur_depth});
 	fb_small2 = new vulkan::FrameBuffer(blur_render_pass, {blur_tex2, blur_depth});
 
-	shader_out = ResourceManager::load_shader("forward/hdr.shader");
-	pipeline_out = new vulkan::Pipeline(shader_out.get(), parent->render_pass(), 0, "3f,3f,2f");
-	pipeline_out->set_culling(0);
-	pipeline_out->rebuild();
-	dset_out = pool->create_set("buffer,sampler,sampler");
+	out = RenderOutData(ResourceManager::load_shader("forward/hdr.shader"), this, {into.fb_main->attachments[0].get(), fb_small2->attachments[0].get()});
 
 
 
@@ -80,31 +118,35 @@ HDRRendererVulkan::HDRRendererVulkan(Renderer *parent) : PostProcessorStage("hdr
 HDRRendererVulkan::~HDRRendererVulkan() {
 }
 
+void HDRRendererVulkan::RenderIntoData::render_into(Renderer *r) {
+	if (!r)
+		return;
+
+	auto cb = r->command_buffer();
+
+	//vb_2d->create_quad(rect::ID_SYM, dynamicly_scaled_source());
+
+	cb->set_viewport(dynamicly_scaled_area(fb_main.get()));
+
+	_render_pass->clear_color = {world.background};
+	cb->begin_render_pass(_render_pass, fb_main.get());
+
+	r->draw();
+
+	cb->end_render_pass();
+}
+
 void HDRRendererVulkan::prepare() {
 	if (child)
 		child->prepare();
 
+	into.render_into(child);
 
 	auto cb = command_buffer();
 
-	vb_2d->create_quad(rect::ID_SYM, dynamicly_scaled_source());
-
-	// into fb_main
-	auto cur = fb_main.get();
-	cb->set_viewport(dynamicly_scaled_area(cur));
-
-	_render_pass->clear_color = {world.background};
-	cb->begin_render_pass(_render_pass, cur);
-
-	if (child)
-		child->draw();
-
-	cb->end_render_pass();
-
-
-	// render blur into fb3!
+	// render blur into fb_small2!
 	PerformanceMonitor::begin(ch_post_blur);
-	process_blur(cb, cur, fb_small1.get(), 1.0f, 0);
+	process_blur(cb, into.fb_main.get(), fb_small1.get(), 1.0f, 0);
 	process_blur(cb, fb_small1.get(), fb_small2.get(), 0.0f, 1);
 	PerformanceMonitor::end(ch_post_blur);
 
@@ -114,13 +156,16 @@ void HDRRendererVulkan::draw() {
 	auto cb = command_buffer();
 
 
-	render_out(cb, fb_main.get(), fb_small2->attachments[0].get());
+	PerformanceMonitor::begin(ch_out);
+	out.render_out(cb, {cam->exposure, cam->bloom_factor, 2.2f, resolution_scale_x, resolution_scale_y});
+	PerformanceMonitor::end(ch_out);
 }
 
 void HDRRendererVulkan::process_blur(CommandBuffer *cb, FrameBuffer *source, FrameBuffer *target, float threshold, int iaxis) {
-	const vec2 AXIS[2] = {{2,0}, {0,1}};
+	const vec2 AXIS[2] = {{(float)BLUR_SCALE,0}, {0,1}};
+	//const float SCALE[2] = {(float)BLUR_SCALE, 1};
 	UBOBlur u;
-	u.radius = cam->bloom_radius * resolution_scale_x;
+	u.radius = cam->bloom_radius * resolution_scale_x * 2 / (float)BLUR_SCALE;
 	u.threshold = threshold / cam->exposure;
 	u.axis = AXIS[iaxis];
 	blur_ubo[iaxis]->update(&u);
@@ -141,29 +186,6 @@ void HDRRendererVulkan::process_blur(CommandBuffer *cb, FrameBuffer *source, Fra
 	cb->end_render_pass();
 
 	//process(cb, {source->attachments[0].get()}, target, shader_blur.get());
-}
-
-void HDRRendererVulkan::render_out(CommandBuffer *cb, FrameBuffer *source, Texture *bloom) {
-	PerformanceMonitor::begin(ch_out);
-
-	cb->bind_pipeline(pipeline_out);
-	dset_out->set_texture(1, source->attachments[0].get());
-	dset_out->set_texture(2, bloom);
-	dset_out->update();
-	cb->bind_descriptor_set(0, dset_out);
-	struct PCOut {
-		matrix p, m, v;
-		float exposure;
-		float bloom_factor;
-		float gamma;
-		float scale_x;
-		float scale_y;
-	};
-	PCOut pco = {matrix::ID, matrix::ID, matrix::ID, cam->exposure, cam->bloom_factor, 2.2f, resolution_scale_x, resolution_scale_y};
-	cb->push_constant(0, sizeof(pco), &pco);
-	cb->draw(vb_2d);
-
-	PerformanceMonitor::end(ch_out);
 }
 
 #endif
--- src/renderer/post/HDRRendererVulkan.h
+++ src/renderer/post/HDRRendererVulkan.h
@@ -19,27 +19,40 @@ public:
 	void prepare() override;
 	void draw() override;
 
-
 	void process_blur(CommandBuffer *cb, FrameBuffer *source, FrameBuffer *target, float threshold, int axis);
-	void render_out(CommandBuffer *cb, FrameBuffer *source, Texture *bloom);
 
-	//shared<RenderPass> render_pass;
-	RenderPass *_render_pass = nullptr;
+	struct RenderIntoData {
+		RenderIntoData() {}
+		RenderIntoData(Renderer *r);
+		void render_into(Renderer *r);
+
+		shared<FrameBuffer> fb_main;
+		DepthBuffer *_depth_buffer = nullptr;
+
+		//shared<RenderPass> render_pass;
+		RenderPass *_render_pass = nullptr;
+	} into;
 
 
-	shared<FrameBuffer> fb_main;
-	DepthBuffer *_depth_buffer = nullptr;
+	struct RenderOutData {
+		RenderOutData(){}
+		RenderOutData(Shader *s, Renderer *r, const Array<Texture*> &tex);
+		void render_out(CommandBuffer *cb, const Array<float> &data);
+		shared<Shader> shader_out;
+		Pipeline* pipeline_out;
+		DescriptorSet *dset_out;
+		VertexBuffer *vb_2d;
+	} out;
+
+
 	shared<FrameBuffer> fb_small1;
 	shared<FrameBuffer> fb_small2;
 
-	FrameBuffer *frame_buffer() const override { return fb_main.get(); };
-	DepthBuffer *depth_buffer() const override { return _depth_buffer; };
+	FrameBuffer *frame_buffer() const override { return into.fb_main.get(); };
+	DepthBuffer *depth_buffer() const override { return into._depth_buffer; };
 	bool forwarding_into_window() const override { return false; };
 
 	shared<Shader> shader_blur;
-	shared<Shader> shader_out;
-	Pipeline* pipeline_out;
-	DescriptorSet *dset_out;
 
 	VertexBuffer *vb_2d;
 
