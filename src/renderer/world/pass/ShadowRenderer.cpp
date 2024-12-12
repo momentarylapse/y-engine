abd454a695cf4c41f6eb80db0d437c7a08564e0b
Michael Ankele <michi@lupina.de>
2024-12-12 02:26:45 +0100

    unified ShadowRenderer

--- src/renderer/world/pass/ShadowRenderer.cpp
+++ src/renderer/world/pass/ShadowRenderer.cpp
@@ -1,13 +1,11 @@
 /*
- * ShadowRendererGL.cpp
+ * ShadowRenderer.cpp
  *
  *  Created on: Dec 11, 2022
  *      Author: michi
  */
 
 #include "ShadowRenderer.h"
-
-#ifdef USING_OPENGL
 
 #ifdef USING_OPENGL
 #include "../geometry/GeometryRendererGL.h"
@@ -24,6 +22,7 @@
 #include <world/Camera.h>
 #include "../../../Config.h"
 
+
 ShadowRenderer::Cascade::Cascade() = default;
 ShadowRenderer::Cascade::~Cascade() = default;
 
@@ -39,6 +38,7 @@ ShadowRenderer::ShadowRenderer() :
 	for (int i=0; i<NUM_CASCADES; i++) {
 		auto& c = cascades[i];
 		c.geo_renderer = new GeometryRenderer(RenderPathType::FORWARD, scene_view);
+		c.geo_renderer->flags = GeometryRenderer::Flags::SHADOW_PASS;
 		c.geo_renderer->material_shadow = material.get();
 
 		shared tex = new Texture(shadow_resolution, shadow_resolution, "rgba:i8");
@@ -47,39 +47,12 @@ ShadowRenderer::ShadowRenderer() :
 #else
 		c.depth_buffer = new DepthBuffer(shadow_resolution, shadow_resolution, "d:f32", true);
 #endif
-		c.texture_renderer = new TextureRenderer({tex, c.depth_buffer});
+		c.texture_renderer = new TextureRenderer({tex, c.depth_buffer}, {"autoclear"});
+		c.texture_renderer->use_params_area = false;
 		c.fb = c.texture_renderer->frame_buffer;
 		c.scale = (i == 0) ? 4.0f : 1.0f;
 		c.texture_renderer->add_child(c.geo_renderer.get());
 	}
-}
-
-void ShadowRenderer::render_cascade(const RenderParams& _params, Cascade& c) {
-	auto params = _params.with_target(c.fb.get());
-	params.desired_aspect_ratio = 1.0f;
-	c.geo_renderer->prepare(params);
-
-	auto m = mat4::scale(c.scale, c.scale, 1);
-	//m = m * jitter(sfb->width*8, sfb->height*8, 1);
-	c.rvd.set_projection_matrix(m * proj);
-	c.rvd.set_view_matrix(mat4::ID);
-
-	// all opaque meshes
-	c.geo_renderer->set(GeometryRenderer::Flags::SHADOW_PASS, c.rvd);
-	//if (true) {
-		c.texture_renderer->render(params);
-	/*} else {
-		nix::bind_frame_buffer(c.fb.get());
-
-		nix::clear_z();
-
-		nix::set_z(true, true);
-
-		//c.texture_renderer->render(params);
-
-		c.geo_renderer->draw(params);
-	}*/
-
 }
 
 void ShadowRenderer::set_scene(SceneView &parent_scene_view) {
@@ -97,4 +70,27 @@ void ShadowRenderer::render(const RenderParams& params) {
 	gpu_timestamp_end(params, ch_prepare);
 	PerformanceMonitor::end(ch_prepare);
 }
+
+void ShadowRenderer::render_cascade(const RenderParams& _params, Cascade& c) {
+	auto params = _params.with_target(c.fb.get());
+	params.desired_aspect_ratio = 1.0f;
+	c.geo_renderer->prepare(params);
+
+#ifdef USING_OPENGL
+	auto m = mat4::scale(c.scale, c.scale, 1);
+#else
+	auto m = mat4::scale(c.scale, -c.scale, 1);
+	c.rvd.index = 0;
 #endif
+	c.rvd.scene_view = &scene_view;
+	c.rvd.ubo.num_lights = 0;
+	c.rvd.ubo.shadow_index = -1;
+	//m = m * jitter(sfb->width*8, sfb->height*8, 1);
+	c.rvd.set_projection_matrix(m * proj);
+	c.rvd.set_view_matrix(mat4::ID);
+
+
+	// all opaque meshes
+	c.geo_renderer->set(GeometryRenderer::Flags::SHADOW_PASS, c.rvd);
+	c.texture_renderer->render(params);
+}
