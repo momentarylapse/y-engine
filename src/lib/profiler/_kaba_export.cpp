cb288fb38a77a67a3bb6522f5145b46fb9ee7b06
Michael Ankele <michi@lupina.de>
2025-08-11 18:03:11 +0200

    [lib/profiler] history

--- src/lib/profiler/_kaba_export.cpp
+++ src/lib/profiler/_kaba_export.cpp
@@ -17,12 +17,16 @@ void export_package_profiler(kaba::Exporter* e) {
 	e->declare_class_element("Profiler.FrameTimingData.gpu", &profiler::FrameTimingData::gpu);
 	e->declare_class_element("Profiler.FrameTimingData.total_time", &profiler::FrameTimingData::total_time);
 
+	e->declare_class_size("Profiler.FrameHistory", sizeof(profiler::FrameHistory));
+	e->declare_class_element("Profiler.FrameHistory.total_times", &profiler::FrameHistory::total_times);
+
 	e->declare_class_size("Profiler", sizeof(profiler::Profiler));
 	e->link_func("Profiler.get_name", &profiler::get_name);
 	e->link("Profiler.avg_frame_time", &profiler::avg_frame_time);
 	e->link("Profiler.frames", &profiler::frames);
 	e->link("Profiler.channels", &profiler::channels);
 	e->link("Profiler.previous_frame_timing", &profiler::previous_frame_timing);
+	e->link("Profiler.history", &profiler::history);
 }
 
 
