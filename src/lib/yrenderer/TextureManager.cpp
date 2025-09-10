305878c49a0ffc5ec22735b0bcfcc8091e901d2b
Michael Ankele <michi@lupina.de>
2025-09-11 00:47:34 +0200

    no error^

--- src/lib/yrenderer/TextureManager.cpp
+++ src/lib/yrenderer/TextureManager.cpp
@@ -41,11 +41,11 @@ shared<ygfx::Texture> TextureManager::load_texture(const Path& filename) {
 
 	Path fn = find_absolute_texture_path(filename);
 	if (fn.is_empty()) {
-		if (ignore_missing_files) {
-			msg_error("missing texture: " + str(filename));
+		//if (ignore_missing_files) {
+			msg_error("missing texture (ignore): " + str(filename));
 			return tex_white;
-		}
-		throw Exception("missing texture: " + str(filename));
+		//}
+		//throw Exception("missing texture: " + str(filename));
 	}
 
 	for (auto&& [key, t]: texture_map)
@@ -66,8 +66,8 @@ shared<ygfx::Texture> TextureManager::load_texture(const Path& filename) {
 		texture_map.add({fn, t});
 		return t;
 	} catch(Exception &e) {
-		if (!ignore_missing_files)
-			throw;
+		//if (!ignore_missing_files)
+		//	throw;
 		msg_error(e.message());
 		return tex_white;
 	}
