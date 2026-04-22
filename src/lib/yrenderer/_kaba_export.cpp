056493e80996bb8859978fc1ce371ef17ed24c5b
Michael Ankele <michi@lupina.de>
2026-04-23 00:04:29 +0200

    [lib/vulkan] Texture.write_float()

--- src/lib/yrenderer/_kaba_export.cpp
+++ src/lib/yrenderer/_kaba_export.cpp
@@ -129,17 +129,7 @@ void texture_write(Texture *t, const Image &im) {
 }
 
 void texture_write_float(Texture *t, const DynamicArray& data) {
-#ifdef USING_VULKAN
-	int n_in = data.num * data.element_size / (int)sizeof(float);
-	int n_tex = t->width * t->height * t->depth;
-	if (n_in != n_tex) {
-		msg_error(format("Texture.write_float(): size mismatch (input: %d - texture: %d)", n_in, n_tex));
-		return;
-	}
-	t->writex(data.data, t->width, t->height, t->depth, "r:f32");
-#else
 	t->write_float(data);
-#endif
 }
 
 void texture_read(Texture* t, Image& im) {
